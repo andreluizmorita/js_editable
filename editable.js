fda2161aba7042e255f2c38fd9c6ec9f3e622956
Andre Luiz Morita <andreluizmorita@gmail.com>
2013-10-02 09:26:45 -0300

    Indent and cleaning script

--- editable.js
+++ editable.js
@@ -1,9 +1,14 @@
 /*
-* PopUp open, print and get content html
+* Edit html and replace in page
 *
-* Copyright (c) 2011 André Morita
+* <script type="text/javascript" src="your_folder_script/jquery.js"></script>
+* <script type="text/javascript" src="your_folder_script/editable.js"></script>
+* <script type="text/javascript">
+* $(function(){	
+*   $("#tag_id").aMEditable();
+* });
+* </script>
 */
-
 $.fn.selectRange = function(start, end) {
     return this.each(function() {
         if (this.setSelectionRange) {
@@ -38,73 +43,63 @@ $.fn.aMEditable = function(options)
 		event.preventDefault();		
 		
 		var defaults = {
-			textarea : 'fit', /* fit */
             background : 'transparent',
             border : 'none',
             inherit : true,
-            action : 'edit', /* edit / post / */
+            action : 'edit',
             url : false,
             id: aMEditable_id + 1,
             offset : $(this).offset()  
-            
 		};        
         
 		var opts = $.extend({}, defaults, options);
         
-        var textarea = 
-            $('<textarea></textarea>')
-                .css({
-                    'width':$(this).width()+'px', 
-                    'height':($(this).height()*2)+'px',
-                    'position':'absolute',
-                    'zIndex':'100000',
-                    'top': opts.offset.top+ 'px',
-                    'left': opts.offset.left + 'px',
-                    'background':opts.background,
-                    'border':opts.border,
-                    'box-shadow':'none',
-                    'overflow':'hidden'  ,
-                    'fontFamily':$(this).css('fontFamily'),
-                    'color':$(this).css('color'),
-                    //'margin':$(this).css('margin'),
-                    'padding':$(this).css('padding'),
-                    'fontSize' : $(this).css('fontSize'),
-                    'fontWeight' : $(this).css('fontWeight'),
-                    'textIndent' : $(this).css('textIndent'),
-                    'textAlign' :$(this).css('textAlign')
-                })
-                .html($.trim($(this).text()))
-                .attr({
-                    'class':'aMEditable-textarea',
-                    'id':'aMEditable-textarea-'+opts.id,
-                    'rel':$(this).attr('id')
-            });
+        var textarea = $('<textarea></textarea>').css({
+			'width':$(this).width()+'px', 
+			'height':($(this).height()*2)+'px',
+			'position': 'absolute',
+			'zIndex':'100000',
+			'top': opts.offset.top+ 'px',
+			'left': opts.offset.left + 'px',
+			'background':opts.background,
+			'border':opts.border,
+			'box-shadow':'none',
+			'overflow':'hidden'  ,
+			'fontFamily':$(this).css('fontFamily'),
+			'color':$(this).css('color'),
+			'padding':$(this).css('padding'),
+			'fontSize' : $(this).css('fontSize'),
+			'fontWeight' : $(this).css('fontWeight'),
+			'textIndent' : $(this).css('textIndent'),
+			'textAlign' :$(this).css('textAlign')
+		})
+		.html($.trim($(this).text()))
+		.attr({
+			'class':'aMEditable-textarea',
+			'id':'aMEditable-textarea-'+opts.id,
+			'rel':$(this).attr('id')
+		});
         
         $(this).html(repeat($.trim($(this).text()).length));
-        
         $('body').append(textarea);
          
         var objId =  $(this);
         
-        $('textarea')
-            .focusout(function() {                    
-                if(this.value != ''){
-                    $(objId).text(this.value).show();
-                    
-                    $(this).remove();
-
-                }
-            })
-            .keydown(function(e){
-                if (e.keyCode == 13 && !e.shiftKey){
-                    e.preventDefault();
-                    $(objId).text(this.value).show();
-                    
-                    $(this).remove();
-                }
-            });
-            
-            $('#aMEditable-textarea-'+opts.id).selectRange(0,$('#aMEditable-textarea-'+opts.id).val().length);
-
+        $('textarea').focusout(function() {                    
+			if(this.value != ''){
+				$(objId).text(this.value).show();
+				
+				$(this).remove();
+			}
+		})
+		.keydown(function(e){
+			if (e.keyCode == 13 && !e.shiftKey){
+				e.preventDefault();
+				$(objId).text(this.value).show();
+				
+				$(this).remove();
+			}
+		}); 
+		$('#aMEditable-textarea-'+opts.id).selectRange(0,$('#aMEditable-textarea-'+opts.id).val().length);
     });
 }
